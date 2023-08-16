351d37548e76a0478dc23289c1171a98ea5b0ce7
pavelTaranov <p.taranov@mail.ru>
2023-08-16 10:58:43 +0300

    Выполнение задания, создание контроллера.

--- java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
+++ java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
@@ -3,7 +3,21 @@ package exercise;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+@RestController
+// Класс контроллера
+public class WelcomeController {
 
-// BEGIN
+    // Сопоставляем GET запрос на адрес /users с обработчиком
+    @GetMapping("/hello")
+    // Обработчик
+    // Привязываем параметр запроса к параметру метода
+    // и задаём значение по умолчанию
+    public String welcomeUser(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello, %s!", name);
+    }
 
-// END
+    @GetMapping("/")
+    public String welcomePage() {
+        return "Welcome to Spring";
+    }
+}
